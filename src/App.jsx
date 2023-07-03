806a618a696530e2a6a4ef006cf9c7e31fe1ea3d
Samnoon1971 <samnoonabrar@gmail.com>
2023-07-03 23:26:33 +0600

    ref #5 Added scss syntax by replacing css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './App.css';
+import './App.scss';
 import axios from 'axios';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 function App() {
