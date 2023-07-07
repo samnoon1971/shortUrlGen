b8e62f44af6a6b6a98f857bca00e7308c766c93e
Samnoon1971 <samnoonabrar@gmail.com>
2023-07-07 10:20:50 +0600

    ref #8 added material UI alerts

--- src/App.jsx
+++ src/App.jsx
@@ -2,58 +2,79 @@ import { useState } from 'react';
 import './App.scss';
 import axios from 'axios';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Alert } from '@mui/material';
+
 function App() {
   const [copyBtn, setCopyBtn] = useState('Copy');
   const [url, setUrl] = useState('');
   const [shortenedUrl, setShortenedUrl] = useState('');
+  const [alert, setAlert] = useState(false);
+  const [alertContent, setAlertContent] = useState("");
+  const [alertSeverity, setAlertSeverity] = useState("");
   const year = new Date().getFullYear();
 
   const handleInput = (input) => {
-    if("Copy" != copyBtn) {
+    if ("Copy" != copyBtn) {
       setCopyBtn("Copy");
     }
+    setCopyBtn("Copied");
+   
     setUrl(input);
   }
   const clipBoard = () => {
     setCopyBtn("Copied");
-    alert("Copied to clipboard!");
-
+    setAlert(true);
+    setAlertSeverity("success");
+    setAlertContent("Copied to clipboard");
   }
   const handleErrors = (error) => {
-    alert("Requested URL Could not be processed.")
+    setAlert(true);
+    setAlertSeverity("error");
+    setAlertContent("Requested URL Could not be processed");
+  
+  }
+  const handleRespone = (res) => {
+    setAlert(true);
+    setAlertSeverity("success");
+    setAlertContent("Short URL generated successfully");
+    setShortenedUrl(res.data.result.full_short_link);
   }
   const shortenUrl = (e) => {
     e.preventDefault();
     axios.get(`https://api.shrtco.de/v2/shorten?url=${url}`)
-    .then(res => setShortenedUrl(res.data.result.full_short_link))
-    .catch(error => handleErrors(error));
-    if("Copy" != copyBtn) {
+      .then(res => handleRespone(res))
+      .catch(error => handleErrors(error));
+    if ("Copy" != copyBtn) {
       setCopyBtn("Copy");
     }
   }
   return (
     <>
+
       <div className='app'>
+
         <div className='shortener'>
           <h2>URL shortener</h2>
           <form onSubmit={shortenUrl}>
             <input
-            placeholder='Enter URL'
-            value={url}
-            onChange={(e) => handleInput(e.target.value)}
+              placeholder='Enter URL'
+              value={url}
+              onChange={(e) => handleInput(e.target.value)}
             />
             <button>Submit</button>
           </form>
-           {
-              shortenedUrl &&
-              <div className='shortener__viewShot'>
-                {shortenedUrl}
-                <CopyToClipboard text={shortenedUrl}>
-                  <button onClick={() => clipBoard()}>{copyBtn}</button>
-                </CopyToClipboard>
-              </div>
-           }
+          {
+            shortenedUrl &&
+            <div className='shortener__viewShot'>
+              {shortenedUrl}
+              <CopyToClipboard text={shortenedUrl}>
+                <button onClick={() => clipBoard()}>{copyBtn}</button>
+              </CopyToClipboard>
+            </div>
+          }
         </div>
+        {alert ? <Alert severity={alertSeverity} onClose={() => setAlert(false)}>{alertContent}</Alert> : <></>}
+
         <footer>{`Copyright © S M Samnoon Abrar ${year}`}</footer>
       </div>
     </>
